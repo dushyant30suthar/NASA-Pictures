944d6bada3b6d4c3507ad9818b559d5f8193cbc6
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-12-10 01:11:38 +0530

    Use coordinator layout as rootLayout of the activity and put toolbar into AppBarLayout. Change primary and secondary color hexs.
    Now toolbar has it's design attributes working. Looks decent.

--- app/src/main/java/com/dushyant30suthar/nasapictures/components/MainActivity.kt
+++ app/src/main/java/com/dushyant30suthar/nasapictures/components/MainActivity.kt
@@ -4,12 +4,32 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.dushyant30suthar.nasapictures.R
+import com.dushyant30suthar.nasapictures.databinding.ActivityMainBinding
 
+/*
+* MainActivity/SingleActivity on which all application screens(Fragments/Views) relays on.*/
 class MainActivity : AppCompatActivity() {
+
+    /*
+    * This project uses ViewBinding not the whole DataBinding stuff. */
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        /*
+        * Let the viewBinding do it's stuff.*/
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        /*
+        * Using toolbar. */
+        setSupportActionBar(binding.mainActivityToolbar)
+
+        /*
+        * Setting up navGraph.
+        * Now flow has been passed to android navigation component.*/
         val navController = this.findNavController(R.id.mainActivityNavHostFragmentContainer)
         navController.setGraph(R.navigation.navigation_cosmos)
     }
