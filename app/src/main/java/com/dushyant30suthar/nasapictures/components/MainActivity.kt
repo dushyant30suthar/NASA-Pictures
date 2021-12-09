0469abb40332c6255d9f6983feaefd96e2bf9b61
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-12-09 18:39:38 +0530

    Add navGraph and configure it in MainActivity.kt

--- app/src/main/java/com/dushyant30suthar/nasapictures/components/MainActivity.kt
+++ app/src/main/java/com/dushyant30suthar/nasapictures/components/MainActivity.kt
@@ -2,11 +2,15 @@ package com.dushyant30suthar.nasapictures.components
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.dushyant30suthar.nasapictures.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.mainActivityNavHostFragmentContainer)
+        navController.setGraph(R.navigation.navigation_cosmos)
     }
 }
