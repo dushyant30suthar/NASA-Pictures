946a686993bb36b6ee18a43b05f05554c01967bd
Dushyant Suthar <dushyant30suthar@gmail.com>
2021-12-11 17:44:33 +0530

    Add entity(data) class for Parsing

--- app/src/main/java/com/dushyant30suthar/nasapictures/domain/cosmosImageList/entities/CosmosImageEntity.kt
+++ app/src/main/java/com/dushyant30suthar/nasapictures/domain/cosmosImageList/entities/CosmosImageEntity.kt
@@ -1,3 +1,25 @@
 package com.dushyant30suthar.nasapictures.domain.cosmosImageList.entities
 
-data class CosmosImageEntity(val url: String)
+
+import com.google.gson.annotations.SerializedName
+
+data class CosmosImageEntity(
+    @SerializedName("date")
+    val date: String = "",
+    @SerializedName("copyright")
+    val copyright: String = "",
+    @SerializedName("media_type")
+    val mediaType: String = "",
+    @SerializedName("hdurl")
+    val hdurl: String = "",
+    @SerializedName("service_version")
+    val serviceVersion: String = "",
+    @SerializedName("explanation")
+    val explanation: String = "",
+    @SerializedName("title")
+    val title: String = "",
+    @SerializedName("url")
+    val url: String = ""
+)
+
+
