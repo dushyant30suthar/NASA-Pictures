47f6309b0ec5543b4be5027b8b4b4af162cd106b
Dushyant Suthar <dushyantsuthar@rankz.io>
2021-12-31 11:56:36 +0530

    Inject CosmosDataStorage and add api methods

--- app/src/main/java/com/dushyant30suthar/nasapictures/data/cosmosImageList/repository/CosmosImageListRepository.kt
+++ app/src/main/java/com/dushyant30suthar/nasapictures/data/cosmosImageList/repository/CosmosImageListRepository.kt
@@ -1,14 +1,21 @@
 package com.dushyant30suthar.nasapictures.data.cosmosImageList.repository
 
+import com.dushyant30suthar.nasapictures.data.cosmosImageList.services.CosmosDataStorage
 import com.dushyant30suthar.nasapictures.data.cosmosImageList.services.CosmosService
 import com.dushyant30suthar.nasapictures.domain.cosmosImageList.entities.CosmosImageEntity
 import io.reactivex.Single
 import javax.inject.Inject
 
 open class CosmosImageListRepository @Inject constructor(
-    private val cosmosService: CosmosService
+    private val cosmosService: CosmosService,
+    private val cosmosDataStorage: CosmosDataStorage
 ) {
+
     fun getCosmosImageList(): Single<List<CosmosImageEntity>> {
         return cosmosService.getCosmosImageList()
+    }
+
+    fun bookCosmosImage(imageId: String) {
+        cosmosDataStorage.bookmarkCosmosImage(imageId)
     }
 }
--- app/src/main/java/com/dushyant30suthar/nasapictures/data/cosmosImageList/services/CosmosDataStorage.kt
+++ app/src/main/java/com/dushyant30suthar/nasapictures/data/cosmosImageList/services/CosmosDataStorage.kt
@@ -3,5 +3,9 @@ package com.dushyant30suthar.nasapictures.data.cosmosImageList.services
 class CosmosDataStorage {
 
 
+    fun bookmarkCosmosImage(imageId: String) {
+
+    }
+
 
 }
